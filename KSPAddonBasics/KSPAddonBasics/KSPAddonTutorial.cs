b41344dab7a8cf0479a6639047505759f8032ed7
jrthom444 <foureyedstudd@gmail.com>
2016-07-16 12:45:33 -0400

    First checkin, simple KSP Addon Template

---
+++ KSPAddonBasics/KSPAddonBasics/KSPAddonTutorial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace KSPAddonBasics
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, false)]
+    class KSPAddonTutorial : MonoBehaviour
+    {
+        private void Log(string message)
+        {
+            Debug.Log("[KSPAddonTutorial]: [#!#]    " + message);
+        }
+        public KSPAddonTutorial()
+        {
+            Log("Constructor called");
+        }
+        void Awake()
+        {
+            Log("Awake Called");
+        }
+        void Start()
+        {
+            Log("Start Called");
+        }
+        void OnDestroy()
+        {
+            Log("Destroy Called");
+        }
+    }
+}
